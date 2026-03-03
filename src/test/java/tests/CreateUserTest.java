e59d7a20f10cec49f12743da35e411eeb98842f1
anastasiafe <anastasia.fedorenko@ediweb.com>
2026-03-03 17:09:58 +0300

    Добавила тесты по созданию и логину пользователя

--- src/test/java/tests/CreateUserTest.java
+++ src/test/java/tests/CreateUserTest.java
@@ -1,4 +1,60 @@
 package tests;
 
+import com.google.gson.Gson;
+import dto.request.CreateUserRequest;
+import dto.response.CreateAndLoginUserResponse;
+import io.restassured.response.Response;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import config.RequestConfig;
+import steps.CheckSteps;
+import steps.SendRequestStep;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 public class CreateUserTest {
+
+    final static String createUserUri = "/auth/register";
+    final static String deleteUserUri = "/auth/user";
+    private final SendRequestStep sendRequestStep = new SendRequestStep();
+    private final Gson gson = new Gson();
+    private final CheckSteps checkSteps = new CheckSteps();
+
+    @Test
+    public void checkCreateUserTest() {
+        CreateUserRequest newUserRequest = new CreateUserRequest("email@emqil.ru", "Qwerty123@", "Тестовый юзер");
+        Response response = sendRequestStep.sendPostRequest(createUserUri, gson.toJson(newUserRequest));
+        checkSteps.checkRequestStatus(response, 200);
+        CreateAndLoginUserResponse newUserResponse = gson.fromJson(response.body().asString(), CreateAndLoginUserResponse.class);
+        response = sendRequestStep.sendDeleteRequest(deleteUserUri, newUserResponse.getAccessToken());
+        checkSteps.checkRequestStatus(response, 202);
+    }
+
+    @Test
+    public void checkCreateUserDublicateErrorTest() {
+        CreateUserRequest newUserRequest = new CreateUserRequest("email@emqil.ru", "Qwerty123@", "Тестовый юзер");
+        Response response = sendRequestStep.sendPostRequest(createUserUri, gson.toJson(newUserRequest));
+        checkSteps.checkRequestStatus(response, 200);
+
+        CreateUserRequest dublicateUserRequest = new CreateUserRequest("email@emqil.ru", "Qwerty123@", "Тестовый юзер");
+        Response dublicateResponse = sendRequestStep.sendPostRequest(createUserUri, gson.toJson(newUserRequest));
+        checkSteps.checkRequestStatus(dublicateResponse, 403);
+        checkSteps.checkRequestErrorMessage("User already exists", dublicateResponse);
+
+        CreateAndLoginUserResponse newUserResponse = gson.fromJson(response.body().asString(), CreateAndLoginUserResponse.class);
+        response = sendRequestStep.sendDeleteRequest(deleteUserUri, newUserResponse.getAccessToken());
+        checkSteps.checkRequestStatus(response, 202);
+    }
+
+    @Test
+    public void checkCreateUserWithoutRequiredFieldErrorTest() {
+        CreateUserRequest newUserRequest = new CreateUserRequest("email", "Qwerty123@", "");
+        Response response = sendRequestStep.sendPostRequest(createUserUri, gson.toJson(newUserRequest));
+        checkSteps.checkRequestStatus(response, 403);
+        checkSteps.checkRequestErrorMessage("Email, password and name are required fields", response);
+    }
+
 }
--- src/test/java/tests/LoginUserTest.java
+++ src/test/java/tests/LoginUserTest.java
@@ -1,4 +1,54 @@
 package tests;
 
+import com.google.gson.Gson;
+import dto.request.CreateUserRequest;
+import dto.request.LoginUserRequest;
+import dto.response.CreateAndLoginUserResponse;
+import dto.response.ErrorResponse;
+import io.restassured.response.Response;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import steps.CheckSteps;
+import steps.SendRequestStep;
+
 public class LoginUserTest {
+
+    private final static String loginUserUri = "/auth/login";
+    final static String createUserUri = "/auth/register";
+    final static String deleteUserUri = "/auth/user";
+    private final Gson gson = new Gson();
+    private final SendRequestStep sendRequestStep = new SendRequestStep();
+    private final CheckSteps checkSteps = new CheckSteps();
+    private CreateUserRequest newUserRequest = new CreateUserRequest("email@emqil.ru", "Qwerty123@", "Тестовый юзер");
+    private CreateAndLoginUserResponse userResponse;
+
+    @BeforeEach
+    void setUp() {
+        Response response = sendRequestStep.sendPostRequest(createUserUri, gson.toJson(newUserRequest));
+        checkSteps.checkRequestStatus(response, 200);
+        userResponse = gson.fromJson(response.body().asString(), CreateAndLoginUserResponse.class);
+    }
+
+    @Test
+    public void checkLoginRequestTest(){
+        LoginUserRequest loginUserRequest = new LoginUserRequest(newUserRequest.getEmail(), newUserRequest.getPassword());
+        Response response = sendRequestStep.sendPostRequest(loginUserUri, gson.toJson(loginUserRequest));
+        checkSteps.checkRequestStatus(response, 200);
+        userResponse = gson.fromJson(response.body().asString(), CreateAndLoginUserResponse.class);
+    }
+
+    @Test
+    public void checkLoginRequestWithWrongDataTest() {
+        LoginUserRequest loginUserRequest = new LoginUserRequest("email@gmail.com", "123456");
+        Response response = sendRequestStep.sendPostRequest(loginUserUri, gson.toJson(loginUserRequest));
+        checkSteps.checkRequestStatus(response, 401);
+        checkSteps.checkRequestErrorMessage("email or password are incorrect", response);
+    }
+
+    @AfterEach
+    void tearDown() {
+        Response response = sendRequestStep.sendDeleteRequest(deleteUserUri, userResponse.getAccessToken());
+        checkSteps.checkRequestStatus(response, 202);
+    }
 }
